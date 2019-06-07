c9faa620ea16c4a1499079dd6ee5529c798ccdfe
Theo <theo@ideine.fr>
2019-06-07 13:28:55 +0200

    Add mainbar to home page

--- src/scenes/Home/Home.jsx
+++ src/scenes/Home/Home.jsx
@@ -1,8 +1,16 @@
+import './home.scss';
+
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import { MainBar } from '../../components/MainBar';
+
 const Home = () => {
-    return <div>TESTING</div>;
+    return (
+        <div>
+            <MainBar content="My Dashbord" shadow toUpperContent />
+        </div>
+    );
 };
 
 Home.propTypes = {};
