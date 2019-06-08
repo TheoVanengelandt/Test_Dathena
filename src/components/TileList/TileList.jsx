67141fa432282ebf280f6941b0d3cf6023727b0f
Theo <theo@ideine.fr>
2019-06-08 20:21:39 +0200

    Refactor TileList : remove title

--- src/components/TileList/TileList.jsx
+++ src/components/TileList/TileList.jsx
@@ -7,26 +7,22 @@ import uuidv4 from 'uuid/v4';
 
 import { Loader } from '../Loader/Loader';
 
-const TileList = ({ data, Title }) => {
+const TileList = ({ data }) => {
     return (
         <React.Fragment>
             {data && (
-                <React.Fragment>
-                    <h2 className="tiles-list-title ">{Title}</h2>
+                <div className="tiles-container">
+                    {data.map(items => (
+                        <Card key={items.id || uuidv4()} className="single-tile">
+                            <h2 className="title">{items.name}</h2>
 
-                    <div className="tiles-container">
-                        {data.map(items => (
-                            <Card key={items.id || uuidv4()} className="single-tile">
-                                <h2 className="title">{items.name}</h2>
-
-                                {/* Display number of file if there at least on file */}
-                                <p className="content">
-                                    {items.total_docs ? `${items.total_docs} files` : 'No files found'}
-                                </p>
-                            </Card>
-                        ))}
-                    </div>
-                </React.Fragment>
+                            {/* Display number of file if there at least on file */}
+                            <p className="content">
+                                {items.total_docs ? `${items.total_docs} files` : 'No files found'}
+                            </p>
+                        </Card>
+                    ))}
+                </div>
             )}
             {!data && <Loader />}
         </React.Fragment>
@@ -35,12 +31,10 @@ const TileList = ({ data, Title }) => {
 
 TileList.propTypes = {
     data: PropTypes.array,
-    Title: PropTypes.string,
 };
 
 TileList.defaultProps = {
     data: [],
-    Title: '',
 };
 
 export { TileList };
